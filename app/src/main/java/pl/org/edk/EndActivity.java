ec638b4ac32b680aa03686d7b645c072c22cb7f1
darekpap <darekpapierniak@gmail.com>
2017-02-27 21:09:36 +0100

    Remove the english translation changes from the master

--- app/src/main/java/pl/org/edk/EndActivity.java
+++ app/src/main/java/pl/org/edk/EndActivity.java
@@ -3,25 +3,14 @@ package pl.org.edk;
 import pl.org.edk.kml.TrackerProvider;
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import java.util.Locale;
-
 public class EndActivity extends Activity {
-
-	LinearLayout endLayout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_end);
-		endLayout = (LinearLayout) findViewById(R.id.endLayout);
-		if (Locale.getDefault().getLanguage().contains("en")) {
-			endLayout.setBackgroundResource(R.drawable.end_screen_en);
-		} else {
-			endLayout.setBackgroundResource(R.drawable.end_screen);
-		}
 		TrackerProvider.dismiss();
 		TempSettings.clear(this);
 	}
