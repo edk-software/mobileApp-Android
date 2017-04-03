b2f9d64b7aa4f84afb133488bd9dea755b700f64
darekpap <darekpapierniak@gmail.com>
2017-04-03 23:58:21 +0200

    Make sure Cursor is released after use

--- app/src/main/java/pl/org/edk/database/services/DbServiceBase.java
+++ app/src/main/java/pl/org/edk/database/services/DbServiceBase.java
@@ -3,6 +3,7 @@ package pl.org.edk.database.services;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
 import pl.org.edk.database.DbHelper;
 import pl.org.edk.database.entities.DbEntityBase;
 
@@ -20,20 +21,20 @@ public class DbServiceBase {
     // ---------------------------------------
     // Public methods
     // ---------------------------------------
-    public static void init(DbHelper dbClient){
+    public static void init(DbHelper dbClient) {
         DbServiceBase.dbClient = dbClient;
     }
 
     // ---------------------------------------
     // Protected methods
     // ---------------------------------------
-    protected long executeQueryInsert(DbEntityBase entity){
+    protected long executeQueryInsert(DbEntityBase entity) {
         ContentValues values = entity.getContentValues();
         return dbWrite().insert(entity.getTableName(), null, values);
     }
 
-    protected int executeQueryUpdate(DbEntityBase entity){
-        if(entity.getId() <= 0)
+    protected int executeQueryUpdate(DbEntityBase entity) {
+        if (entity.getId() <= 0)
             return 0;
 
         ContentValues values = entity.getContentValues();
@@ -41,47 +42,47 @@ public class DbServiceBase {
         return dbWrite().update(entity.getTableName(), values, whereClause, new String[]{String.valueOf(entity.getId())});
     }
 
-    protected int executeQueryUpdate(DbEntityBase entity, String comparisonColumn, String value){
+    protected int executeQueryUpdate(DbEntityBase entity, String comparisonColumn, String value) {
         ContentValues values = entity.getContentValues();
         String whereClause = comparisonColumn + " = ? ";
         return dbWrite().update(entity.getTableName(), values, whereClause, new String[]{value});
     }
 
-    protected int executeQueryUpdate(DbEntityBase entity, ArrayList<String> whereColumns, String[] whereArgs){
-        if(entity.getId() <= 0)
+    protected int executeQueryUpdate(DbEntityBase entity, ArrayList<String> whereColumns, String[] whereArgs) {
+        if (entity.getId() <= 0)
             return 0;
 
         ContentValues values = entity.getContentValues();
 
         // Format the statement
         String whereStatement = "";
-        for(int i = 0; i < whereColumns.size(); i++){
+        for (int i = 0; i < whereColumns.size(); i++) {
             String column = whereColumns.get(i);
             whereStatement += column + " = ? ";
-            if(i + 1 < whereColumns.size())
+            if (i + 1 < whereColumns.size())
                 whereStatement += "AND ";
         }
 
         return dbWrite().update(entity.getTableName(), values, whereStatement, whereArgs);
     }
 
-    protected Cursor executeQueryGetAll(String tableName, String[] columns){
+    protected Cursor executeQueryGetAll(String tableName, String[] columns) {
         return dbRead().query(tableName, columns, null, null, null, null, null);
     }
 
-    protected Cursor executeQueryWhere(String tableName, String[] columns, ArrayList<String> whereColumns, String[] whereArgs){
+    protected Cursor executeQueryWhere(String tableName, String[] columns, ArrayList<String> whereColumns, String[] whereArgs) {
         // Format the statement
         String whereStatement = "";
-        for(int i = 0; i < whereColumns.size(); i++){
+        for (int i = 0; i < whereColumns.size(); i++) {
             String column = whereColumns.get(i);
             whereStatement += column + " = ? ";
-            if(i + 1 < whereColumns.size())
+            if (i + 1 < whereColumns.size())
                 whereStatement += "AND ";
         }
         return dbRead().query(tableName, columns, whereStatement, whereArgs, null, null, null);
     }
 
-    protected Cursor executeQueryWhere(String tableName, String[] columns, String interestColumn, String interestValue){
+    protected Cursor executeQueryWhere(String tableName, String[] columns, String interestColumn, String interestValue) {
         // Put the single values into containers
         ArrayList<String> whereColumns = new ArrayList<>();
         whereColumns.add(interestColumn);
@@ -90,14 +91,32 @@ public class DbServiceBase {
         return executeQueryWhere(tableName, columns, whereColumns, whereValues);
     }
 
+    protected <T> T Execute(CursorCommand<T> command) {
+        Cursor cursor = null;
+        try {
+            cursor = command.GetCursor();
+            return command.Run(cursor);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+    protected interface CursorCommand<T> {
+        Cursor GetCursor();
+
+        T Run(Cursor cursor);
+    }
+
     // ---------------------------------------
     // Private methods
     // ---------------------------------------
-    private SQLiteDatabase dbRead(){
+    private SQLiteDatabase dbRead() {
         return dbClient.getReadableDatabase();
     }
 
-    private SQLiteDatabase dbWrite(){
+    private SQLiteDatabase dbWrite() {
         return dbClient.getWritableDatabase();
     }
 }
